110296de6bbbac8c1979de07ee38ce08bb815cff
Brenda Vammen <bvammen@spp.org>
2018-08-23 14:15:57 -0500

    Reset with yarn

--- src/examples/Hello.test.js
+++ src/examples/Hello.test.js
@@ -1,5 +1,34 @@
-describe("When setting up testing", () => {
-    it("should fail", () => {
-        expect(1 + 1).toBe(2);
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Hello(props) {
+    return <h1>Hello at {props.now}</h1>;
+}
+
+const moment = new Date(1488946400000);
+
+describe("When testing directly", () => {
+    let result;
+    beforeAll(() => {
+        result = Hello({now: moment.toISOString()});
+    });
+
+    it("return a value", () => {
+        expect(result).not.toBeNull();
+    });
+
+    it("is a h1", () => {
+        expect(result.type).toBe("h1");
+    });
+
+    it("has children", () => {
+        expect(result.props.children).toBeTruthy();
+    });
+});
+
+describe("When testing with ReactDOM", () => {
+    it("renders without crashing", () => {
+        const div = document.createElement("div");
+        ReactDOM.render(<Hello now={moment.toISOString()} />, div);
     });
 });
